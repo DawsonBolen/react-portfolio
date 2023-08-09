f5def3fa7559770426fc4012b0dac54b6c741825
DawsonBolen <dawson120@live.com>
2023-08-09 11:11:10 -0700

    now running into issues

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,22 +4,22 @@ export default function Footer() {
     return (
         <footer>
 
-            <a class="footer-link" href="mailto: dawson120@live.com">
-                <div class="footer-contact">
+            <a className="footer-link" href="mailto: dawson120@live.com">
+                <div className="footer-contact">
                     <img src="images/emailicon.png" width="25px" height="25px" alt="email"></img>
-                    <p class="footerpar">dawson120@live.com</p>
+                    <p className="footerpar">dawson120@live.com</p>
                 </div>
             </a>
-            <a class="footer-link" href="tel:4257499555">
-                <div class="footer-contact">
+            <a className="footer-link" href="tel:4257499555">
+                <div className="footer-contact">
                     <img src="images/phoneicon.png" width="25px" height="25px" alt="phone"></img>
-                    <p class="footerpar">425-749-9555</p>
+                    <p className="footerpar">425-749-9555</p>
                 </div>
             </a>
-            <a class="footer-link" href="https://www.linkedin.com/in/dawsonbolen/">
-                <div class="footer-contact">
+            <a className="footer-link" href="https://www.linkedin.com/in/dawsonbolen/">
+                <div className="footer-contact">
                     <img src="images/linkedinicon.png" width="25px" height="25px" alt="linkedin"></img>
-                    <p class="footerpar">Dawson Bolen</p>
+                    <p className="footerpar">Dawson Bolen</p>
                 </div>
             </a>
 
--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -10,7 +10,7 @@ const styles = {
         paddingTop: '56.25%',
     },
 
-};
+}
 
 
 
