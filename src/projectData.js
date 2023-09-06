137094a5e0c0ed1e67bbe6924069730d85e71b38
DawsonBolen <dawson120@live.com>
2023-09-06 15:39:15 -0700

    added changes to some of my web projects. I transfered connect four to github pages so i had to change the links, also added new explaination for ez-trade.

--- src/projectData.js
+++ src/projectData.js
@@ -1,35 +1,46 @@
 
 
 export const projectsArray = [
+
     {
         id: 1,
-        title: 'Connect 4',
-        description: "I created a connect 4 game for a class using HTML, CSS, and JavaScript. Press the button to view the site and play the game. I had this hosted on reclaim hosting and am currently transfering it to github so I don't have to pay for hosting. I will have it up soon but for now it is down if you press the link. ",
-        link: 'https://dawson-bolen.com/connect4/index.html',
-        image: '/project-images/connect-four.jpg',
+        title: 'EZ Trade',
+        description: "EZ trade was a full stack ecommerce site that I made along with a group for a project. We used Handlebars, JavaScript, Express, Node, and CSS. I helped slightly with routing and express, but my role was primarily front end. I did the majority of the front end work. I did all of the design and css, created all of our graphics using adobe Illustrator, designed the logo, and did the majority of the front-end JavaScript and handlebars. We used live share, so it probably looks like one member did all of the pushes, but everyone contributed. The project manager took it down so he wouldn't be charged so you can clone the repo and run npm i and npm run dev to view our site in action. I will work to get a walkthrough video up in the next week or so.  ",
+        image: '/project-images/ez-trade.jpg',
+        repo: 'https://github.com/robagraham808/Ez-trade'
     },
     {
         id: 2,
-        title: 'NAMI',
-        description: 'I worked with a group to create a website for Nami North Central Idaho. I helped design it and was in charge of the technical side of the project as well. Unfortunately, they are no longer around so the site is no longer there. Their organization just merged with NAMI Idaho. It is unfortunate, however, this was a great learning experience to actually work for a real client.',
-        image: '/images/namireal.jpg',
+        title: 'Square Share',
+        description: "This was our final project for my coding bootcamp. We created a social networking app called square share. It is a full stack MERN application. You can create discussion threads and add post to those threads. Additionally, you can add comments. My role was primarily front end. I helped a little bit with graph and would have to work with it to get the front end to work properly. I did the majority of the react and all of the css. Additionally, I designed it and created our graphics in illustrator. In addition to the MERN stack, we also used graphql, apollo client, and jwt web tokens for authentication. ",
+        link: 'https://sharesquare-c95734f95f40.herokuapp.com/Home',
+        image: '/project-images/square-share.jpg',
+        repo: 'https://github.com/DawsonBolen/project-3'
     },
     {
         id: 3,
+        title: 'Connect 4',
+        description: "I created a connect 4 game for a class using HTML, CSS, and JavaScript. This was one of my first major projects for school several years ago, so i may decide to improve it since I've learned a lot since then. Press the button to view the site and play the game.  ",
+        link: 'https://dawsonbolen.github.io/Connect-4/',
+        image: '/project-images/connect-four.jpg',
+        repo: 'https://github.com/DawsonBolen/Connect-4'
+    },
+    {
+        id: 4,
         title: 'St Vincent De Paul',
         description: 'I worked in a group to create a mockup website for St. Vincent de Paul. Their current website was super outdated and they wanted new ideas. We also conducted UX research before finalizing our designs. I was responsible for our creating personas and creating the partners page. Click the button to view the full adobe XD document. ',
         link: 'https://xd.adobe.com/view/c792ce5e-a6d5-4bb0-86dd-2231d00e27f3-92c1/?fullscreen',
         image: '/project-images/st-vincent.jpg',
     },
     {
-        id: 4,
+        id: 5,
         title: 'Peace Of Heart',
         description: 'I worked for Peace of Heart therapy doing several tasks. I designed their logo and buisness card as well as created and designed their website. I helped them use vista print to order custom pens and cards alike. It was a good experience working for them. ',
         link: 'https://www.peaceofheart-therapy.com/',
         image: '/project-images/peace-of-heart.jpg',
     },
     {
-        id: 5,
+        id: 6,
         title: 'Coding Quiz',
         description: "I created a coding quiz for a class. It's 5 questions long and is timed. It also uses local storage to display the past scores on the last page. I made this using html, css, and JavaScript. ",
         link: 'https://dawsonbolen.github.io/coding-quiz/',
@@ -37,7 +48,7 @@ export const projectsArray = [
         repo: 'https://github.com/DawsonBolen/coding-quiz'
     },
     {
-        id: 6,
+        id: 7,
         title: 'NBA Highlights',
         description: "This was a group project in my coding bootcamp. We created an NBA Highlights app that allows the user to search up a team, get that teams record, and get game stats for every game of their season. Additionally, it used the youtube api to retrieve relevant videos. My personal job was being the main person in charge of the NBA Api. It was a very simple api which limitied what we could do. It just returned raw text data, however, because of this I really got to practice and refine my skills with JavaScript logic. Try it out for yourself. ",
         link: 'https://rau1avi1a.github.io/NBA-Highlights/',
@@ -45,7 +56,7 @@ export const projectsArray = [
         repo: 'https://github.com/rau1avi1a/NBA-Highlights',
     },
     {
-        id: 7,
+        id: 8,
         title: 'Workday Scheduler',
         description: "This was an assignment in my coding bootcamp class. We took existing html and css, but added our own J Querry code to create a workday scheduler. This used many concepts such as J Querry and local storage. ",
         link: 'https://dawsonbolen.github.io/workday-scheduler/',
@@ -53,7 +64,7 @@ export const projectsArray = [
         repo: 'https://github.com/DawsonBolen/workday-scheduler',
     },
     {
-        id: 8,
+        id: 9,
         title: 'Portfolio',
         description: "I think it goes without saying that I made this porfolio site myself. I included this so you could see the repo if you wanted to view the code. I originally made a multi page portfolio but then converted it to React in order to practice my react skills. I wanted to mention that on top of the work I've shown, every Icon, and Graphic I have created myself using mainly Illustrator, with an exception to lots of the logos in the skills section of my about page. If you would also like to see my old portfolio, I have that linked on the README. ",
         link: 'https://dawsonbolen.github.io/react-portfolio/',
@@ -62,7 +73,7 @@ export const projectsArray = [
 
     },
     {
-        id: 9,
+        id: 10,
         title: 'Random Password',
         description: "This assignment was also for my coding bootcamp. We created this Random Password Generator from starting source code and added JavaScript to make it work correctly. ",
         link: 'https://dawsonbolen.github.io/Random-Password/',
@@ -70,7 +81,7 @@ export const projectsArray = [
         repo: 'https://github.com/DawsonBolen/Random-Password'
     },
     {
-        id: 10,
+        id: 11,
         title: 'README Generator',
         description: "This assignment was also for my coding bootcamp. We created this README.md Generator from starting source code and added JavaScript to make it work correctly. It uses node.js and inquirerer to build a readme file based on user input. It is a command line app and is not deployed so instead of a link, I added a walkthrough video below. ",
         link: 'https://drive.google.com/file/d/1qITiMJ0AB_C-JX3H-7F0AYuoU7hmkI64/view',
@@ -78,7 +89,7 @@ export const projectsArray = [
         repo: 'https://github.com/DawsonBolen/readme-generator'
     },
     {
-        id: 11,
+        id: 12,
         title: 'SVG Logo Generator',
         description: "This is an app that generates an svg logo from the command line. This was built with node.js and inquirer to take user input in order to generate an svg logo. The user can choose the shape, shape color, text, and text color to create a custom logo. I used an object oriented approach to this assignment. Once again, I included a walkthrough video below. ",
         link: 'https://drive.google.com/file/d/1FPmaqyTL9zud4SLVjRlqRFWyjJyVFi0g/view',
@@ -86,7 +97,7 @@ export const projectsArray = [
         repo: 'https://github.com/DawsonBolen/SVG-Logo-Generator'
     },
     {
-        id: 12,
+        id: 13,
         title: 'Note Taker',
         description: "This is an app that generates an svg logo from the command line. This was built with node.js and inquirer to take user input in order to generate an svg logo. The user can choose the shape, shape color, text, and text color to create a custom logo. I used an object oriented approach to this assignment. Once again, I included a walkthrough video below. ",
         link: 'https://note----taking--app.herokuapp.com/',
@@ -94,20 +105,12 @@ export const projectsArray = [
         repo: 'https://github.com/DawsonBolen/Note-Taker'
     },
     {
-        id: 13,
+        id: 14,
         title: 'Employee Tracker',
         description: "This is an employee tracker app that uses node.js, express.js, and sql. You can add employees, add roles, add departments, and add managers as well as delete and edit each of these as well. It is a command line app so it is not deployed. You may view the repo and the walkthrough video however. ",
         link: 'https://drive.google.com/file/d/1zTUW1KpqgFj_wzRFARDnKDPvXZlXFnKh/view',
         image: '/project-images/employee-tracker.jpg',
         repo: 'https://github.com/DawsonBolen/Employee-Tracker'
-    },
-    {
-        id: 14,
-        title: 'EZ Trade',
-        description: "This was our second group project in my coding bootcamp. We came up with EZ-Trade, an ecommerce site that we made for this project. We don't actually sell anything, but it is made as a mock up of a full-stack ecommerce site. We used node.js, express, sql, sequelize, and handlebars to make the site. Everything works, but keep in mind we were limited to two weeks to complete this. Not everything is seeded, the only categories with products so far are automotive and shoes. You can even post products, but you need to post the link of an image, we didn't have enough time to incorporate an image drop or upload API. I was responsible for the design, all the css, and the majority of the front-end JavaScript. I also helped slightly with models and routing. I created the logos and graphics in Illustrator. We also used liveshare so I don't have many commits, I would add them when we met during live share and the project manager would push them. Our project manager took it down from Heroku so it wouldn't charge him. To see our site in action you will have to clone repo and run npm i and npm run dev. I will try to contact my group and try to get a walkthrough video created. ",
-        link: 'https://ez-trade-project-f4108a29ac96.herokuapp.com/',
-        image: '/project-images/ez-trade.jpg',
-        repo: 'https://github.com/robagraham808/Ez-trade'
     },
     {
         id: 15,
@@ -119,12 +122,10 @@ export const projectsArray = [
     },
     {
         id: 16,
-        title: 'Square Share',
-        description: "This was our final project for my coding bootcamp. We created a social networking app called square share. It is a full stack MERN application. You can create discussion threads and add post to those threads. Additionally, you can add comments. My role was primarily front end. I helped a little bit with graph and would have to work with it to get the front end to work properly. I did the majority of the react and all of the css. Additionally, I designed it and created our graphics in illustrator. In addition to the MERN stack, we also used graphql, apollo client, and jwt web tokens for authentication. ",
-        link: 'https://sharesquare-c95734f95f40.herokuapp.com/Home',
-        image: '/project-images/square-share.jpg',
-        repo: 'https://github.com/DawsonBolen/project-3'
-    }
+        title: 'NAMI',
+        description: 'I worked with a group to create a website for Nami North Central Idaho. I helped design it and was in charge of the technical side of the project as well. Unfortunately, they are no longer around so the site is no longer there. Their organization just merged with NAMI Idaho. It is unfortunate, however, this was a great learning experience to actually work for a real client.',
+        image: '/images/namireal.jpg',
+    },
 ]
 
 
